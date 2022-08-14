552e3faa7f5284b3f7417b7525bf259fd526807c
zhangwei <nstl1999@163.com>
2022-08-14 21:57:49 +0800

    初次完成开发和测试

---
+++ test/expected/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="gx-table">
+        <a class="gx-tree">
+            <i>
+                <span class="gx-iicon"></span>
+            </i>
+        </a>
+    </div>
+</template>
---
+++ test/fixtures/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="el-table">
+        <a class="el-tree">
+            <i >
+                <span class="el-iicon"></span>
+            </i>
+        </a>
+    </div>
+</template>
